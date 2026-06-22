96de027eea7db5a62b1bf2367afa01605825468b
JonasGropp <65590269+JonasGropp@users.noreply.github.com>
2026-06-22 11:01:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
   const video = document.getElementById('weddingVideo');
+  const soundButton = document.getElementById('soundButton');
+
+  soundButton.addEventListener('click', () => {
+
+  if (video.muted) {
+
+    video.muted = false;
+    video.volume = 1;
+
+    soundButton.innerHTML = '🔊 Ton an';
+
+  } else {
+
+    video.muted = true;
+
+    soundButton.innerHTML = '🔇 Ton einschalten';
+
+  }
+
+});
+  
   const videoWrapper = document.getElementById('videoWrapper');
 
   videoWrapper.addEventListener('click', () => {

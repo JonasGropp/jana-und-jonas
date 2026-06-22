21c73dc27364e04971d599cd28a314a241479f4d
JonasGropp <65590269+JonasGropp@users.noreply.github.com>
2026-06-22 08:03:26 +0200

    Delete jana-und-jonas directory

--- jana-und-jonas/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const video = document.getElementById('weddingVideo');
-  const videoWrapper = document.getElementById('videoWrapper');
-
-  videoWrapper.addEventListener('click', () => {
-    video.currentTime = 0;
-    video.play().catch(() => {});
-  });
-});

aef1a3bebcf9ee7fcde32b9c8a1ebf1ee5b0984f
JonasGropp <65590269+JonasGropp@users.noreply.github.com>
2026-06-25 11:33:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -23,10 +23,10 @@ document.addEventListener('DOMContentLoaded', () => {
   ]);
 
   const videoScrollTimeline = [
-    { time: 12, progress: 0.04 },
-    { time: 13, progress: 0.30 },
-    { time: 14, progress: 0.61 },
-    { time: 18, progress: 0.73 }
+    { time: 11.5, progress: 0.04 },
+    { time: 12.5, progress: 0.40 },
+    { time: 13.5, progress: 0.40 },
+    { time: 18, progress: 0.40 }
   ];
 
   function clamp(value, min, max) {
